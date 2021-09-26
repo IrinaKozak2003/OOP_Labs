5e62833cce91299b88aa869bf76cc33be74f0694
Pomidorka <030503ira@gmail.com>
2021-09-26 14:41:29 +0300

    dp2

--- OOP_Lab2/OOP_Lab2/Program.cs
+++ OOP_Lab2/OOP_Lab2/Program.cs
@@ -10,41 +10,41 @@ namespace OOTP_Lab2
         static void Main(string[] args)
         {
             // Ввод и вывод встроенных типов С#
-            Console.WriteLine("Input Int:");
-            int Integer = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"Int {Integer}");
+            //Console.WriteLine("Input Int:");
+            //int Integer = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine($"Int {Integer}");
 
-            Console.WriteLine("Input Byte:");
-            byte Byte = Convert.ToByte(Console.ReadLine());
-            Console.WriteLine($"Byte {Byte}");
+            //Console.WriteLine("Input Byte:");
+            //byte Byte = Convert.ToByte(Console.ReadLine());
+            //Console.WriteLine($"Byte {Byte}");
 
-            Console.WriteLine("Input Char:");
-            char Char = Convert.ToChar(Console.ReadLine());
-            Console.WriteLine($"Char {Char}");
+            //Console.WriteLine("Input Char:");
+            //char Char = Convert.ToChar(Console.ReadLine());
+            //Console.WriteLine($"Char {Char}");
 
-            Console.WriteLine("Input Double:");
-            double Double = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine($"Double {Double}");
+            //Console.WriteLine("Input Double:");
+            //double Double = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine($"Double {Double}");
 
-            Console.WriteLine("Input Bool:");
-            bool Bool = Convert.ToBoolean(Console.ReadLine());
-            Console.WriteLine($"Bool {Bool}");
+            //Console.WriteLine("Input Bool:");
+            //bool Bool = Convert.ToBoolean(Console.ReadLine());
+            //Console.WriteLine($"Bool {Bool}");
 
-            Console.WriteLine("Input Float:");
-            float Float = Convert.ToSingle(Console.ReadLine());
-            Console.WriteLine($"Float {Float}");
+            //Console.WriteLine("Input Float:");
+            //float Float = Convert.ToSingle(Console.ReadLine());
+            //Console.WriteLine($"Float {Float}");
 
-            Console.WriteLine("Input Long:");
-            long Long = Convert.ToInt64(Console.ReadLine());
-            Console.WriteLine($"Long {Long}");
+            //Console.WriteLine("Input Long:");
+            //long Long = Convert.ToInt64(Console.ReadLine());
+            //Console.WriteLine($"Long {Long}");
 
-            Console.WriteLine("Input String:");
-            string String = Convert.ToString(Console.ReadLine());
-            Console.WriteLine($"String {String}");
+            //Console.WriteLine("Input String:");
+            //string String = Convert.ToString(Console.ReadLine());
+            //Console.WriteLine($"String {String}");
 
-            Console.WriteLine("Input Short:");
-            short Short = Convert.ToInt16(Console.ReadLine());
-            Console.WriteLine($"Short {Short}");
+            //Console.WriteLine("Input Short:");
+            //short Short = Convert.ToInt16(Console.ReadLine());
+            //Console.WriteLine($"Short {Short}");
 
             ushort Ushort = 7456;
             Console.WriteLine($"Ushort {Ushort}");
@@ -67,9 +67,9 @@ namespace OOTP_Lab2
 
             //Неявное преобразование типов
             int integer = Ushort;
-            float fl = Integer;
-            short sh = Byte;
-            decimal dec = Long;
+          //  float fl = Integer;
+           // short sh = Byte;
+            //decimal dec = Long;
             long lg = Uint;
 
             //Явное преобразование типов
@@ -77,13 +77,91 @@ namespace OOTP_Lab2
             long b = (Int64)Ulong;
             short c = (Int16)Ushort;
             byte d = (Byte)Sbyte;
-            double s = (Double)Float;
+            //double s = (Double)Float;
 
             //Упаковка и распаковка значимых типов
+            int k = 1;
+            object l = k;
+            int o = (int)l;
+
+            char notobj = 'i';
+            object srtr = notobj;
+            char hjg = (char)srtr;
+
+            long m = 8767;
+            object j = m;
+            long h = (long)j;
+
+            var v = 8767;
+            object kh = v;
+            char p = (char)k;
+
+            //Неявно типизированная перенная
+            var vds = 1;
+            vds = '7';
+
+            Nullable<int> z2 = 5;
+            Nullable<bool> enabled2 = null;
+
+            string str = "dfhhfs";
+            string str1 = "dfhhfs";
+
+            string str2 = "string1";
+            string str3 = "string2";
+            string str4 = "string3";
+
+            if (str == str1)
+            {
+                Console.WriteLine($"str == str1");
+            }
+
+            string str6 = str2 + " " + str3;
+            Console.WriteLine($"str2 + str3 == {str6}");
+    
+            string str7 = string.Copy(str2);
+            Console.WriteLine(str7);
+
+            string str8 = str4.Substring(3, 2);
+            Console.WriteLine($"Substring {str8}");
+
+            string s14 = "Эта строка, которая будет разделена на подстроки с использованием разделителей";
+            string[] res = s14.Split(new char[] { ' ', ',' });
+            foreach (string str14 in res)
+            {
+                Console.WriteLine(str14);
+            }
+
+            string str98 = str3.Insert(2, "str");
+            Console.WriteLine(str98);
+
+            string str87 = str3.Remove(2, 3);
+   
+            Console.WriteLine(str87);
+            string empty = "";
+            string Null = null;
+            Console.WriteLine(string.IsNullOrEmpty(empty));
+            Console.WriteLine(string.IsNullOrEmpty(Null));
+            if(empty == Null)
+            {
+                Console.WriteLine("empty == null");
+            }
+            else
+            {
+                Console.WriteLine("empty != null");
+            }
+            Console.WriteLine(empty+ "string");
+
+            //String builder
+            StringBuilder SB = new StringBuilder("string", 50);
+            SB.Append(new char[] { ' ','b', 'u' });
+            SB.AppendFormat("ilding");
+            Console.WriteLine(SB.ToString());
+
+            //Arreys
+
 
             Console.ReadKey();
-            //
-            //hgddhfg
+            
         }
     }
 }
