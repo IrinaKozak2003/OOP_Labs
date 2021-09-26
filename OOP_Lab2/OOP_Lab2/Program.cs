196e3b0d064b993960f921742f6649a66fb6890a
Pomidorka <030503ira@gmail.com>
2021-09-26 20:44:27 +0300

    commit3

--- OOP_Lab2/OOP_Lab2/Program.cs
+++ OOP_Lab2/OOP_Lab2/Program.cs
@@ -10,41 +10,41 @@ namespace OOTP_Lab2
         static void Main(string[] args)
         {
             // Ввод и вывод встроенных типов С#
-            //Console.WriteLine("Input Int:");
-            //int Integer = Convert.ToInt32(Console.ReadLine());
-            //Console.WriteLine($"Int {Integer}");
+            Console.WriteLine("Input Int:");
+            int Integer = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Int {Integer}");
 
-            //Console.WriteLine("Input Byte:");
-            //byte Byte = Convert.ToByte(Console.ReadLine());
-            //Console.WriteLine($"Byte {Byte}");
+            Console.WriteLine("Input Byte:");
+            byte Byte = Convert.ToByte(Console.ReadLine());
+            Console.WriteLine($"Byte {Byte}");
 
-            //Console.WriteLine("Input Char:");
-            //char Char = Convert.ToChar(Console.ReadLine());
-            //Console.WriteLine($"Char {Char}");
+            Console.WriteLine("Input Char:");
+            char Char = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine($"Char {Char}");
 
-            //Console.WriteLine("Input Double:");
-            //double Double = Convert.ToDouble(Console.ReadLine());
-            //Console.WriteLine($"Double {Double}");
+            Console.WriteLine("Input Double:");
+            double Double = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Double {Double}");
 
-            //Console.WriteLine("Input Bool:");
-            //bool Bool = Convert.ToBoolean(Console.ReadLine());
-            //Console.WriteLine($"Bool {Bool}");
+            Console.WriteLine("Input Bool:");
+            bool Bool = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine($"Bool {Bool}");
 
-            //Console.WriteLine("Input Float:");
-            //float Float = Convert.ToSingle(Console.ReadLine());
-            //Console.WriteLine($"Float {Float}");
+            Console.WriteLine("Input Float:");
+            float Float = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine($"Float {Float}");
 
-            //Console.WriteLine("Input Long:");
-            //long Long = Convert.ToInt64(Console.ReadLine());
-            //Console.WriteLine($"Long {Long}");
+            Console.WriteLine("Input Long:");
+            long Long = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine($"Long {Long}");
 
-            //Console.WriteLine("Input String:");
-            //string String = Convert.ToString(Console.ReadLine());
-            //Console.WriteLine($"String {String}");
+            Console.WriteLine("Input String:");
+            string String = Convert.ToString(Console.ReadLine());
+            Console.WriteLine($"String {String}");
 
-            //Console.WriteLine("Input Short:");
-            //short Short = Convert.ToInt16(Console.ReadLine());
-            //Console.WriteLine($"Short {Short}");
+            Console.WriteLine("Input Short:");
+            short Short = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine($"Short {Short}");
 
             ushort Ushort = 7456;
             Console.WriteLine($"Ushort {Ushort}");
@@ -67,9 +67,9 @@ namespace OOTP_Lab2
 
             //Неявное преобразование типов
             int integer = Ushort;
-          //  float fl = Integer;
-           // short sh = Byte;
-            //decimal dec = Long;
+            float fl = Integer;
+            short sh = Byte;
+            decimal dec = Long;
             long lg = Uint;
 
             //Явное преобразование типов
@@ -77,7 +77,7 @@ namespace OOTP_Lab2
             long b = (Int64)Ulong;
             short c = (Int16)Ushort;
             byte d = (Byte)Sbyte;
-            //double s = (Double)Float;
+            double s = (Double)Float;
 
             //Упаковка и распаковка значимых типов
             int k = 1;
@@ -159,9 +159,138 @@ namespace OOTP_Lab2
 
             //Arreys
 
+            int[,] mas = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 }, { 10, 11, 12 } };
 
+            for (int i = 0; i < 4; i++)
+            {
+                for (int n = 0; n < 3; n++)
+                {
+                    Console.Write("{0}\t", mas[i, n]);
+                }
+                Console.WriteLine();
+            }
+            string[] str_arr =  { "str1", "str2", "str3", "str4", "str5" };
+            for (int i = 0; i < str_arr.Length; i++)
+            {
+                Console.WriteLine(str_arr[i] + '\t');
+            }
+            str_arr[3] = "str";
+
+            Console.WriteLine('\n');
+
+            for (int i = 0; i < str_arr.Length; i++)
+            {
+                Console.WriteLine(str_arr[i] + '\t');
+            }
+
+            Console.WriteLine("Array lenght: " + str_arr.Length);
+
+
+            int[][] jag_arr = new int[3][];
+            jag_arr[0] = new int[2]; 
+            jag_arr[1] = new int[3]; 
+            jag_arr[2] = new int[4];
+
+            for (int i = 0; i < jag_arr.Length; i++)
+            {
+                for (int y = 0; y < jag_arr[i].GetLength(0); y++)
+                {
+                    int v1 = Convert.ToInt32(Console.ReadLine());
+                    jag_arr[i][y] = v1;
+                }
+            }
+
+            for (int i = 0; i < jag_arr.Length; i++)
+            {
+                for (int y = 0; y < jag_arr[i].GetLength(0); y++)
+                {
+                    Console.Write(jag_arr[i][y] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            var t = jag_arr;
+            var s12 = str2;
+
+            //Кортежи
+
+            (int, string, char, string, ulong) tuple = (12, "string", 'c', "string1", 346374);
+
+            // риспаковка кортежей
+
+            Console.WriteLine(tuple);
+            Console.WriteLine(tuple.Item1);
+            Console.WriteLine(tuple.Item3);
+            Console.WriteLine(tuple.Item5);
+
+            int e = tuple.Item1;
+            string st= tuple.Item2;
+            char ch= tuple.Item3;
+            string str2000 = tuple.Item4;
+            ulong ul = tuple.Item5;
+
+             void Deconstruct(out int x, out string y, out char ch, out string str9, ulong u ) { x = tuple.Item1; y = tuple.Item2; ch = tuple.Item3; str9 = tuple.Item4; u = tuple.Item5;}
+
+
+            (int, int) e1 = (1, 1);
+            (int, int) e2 = (1, 1);
+
+             if(e1 == e2)
+            {
+                Console.WriteLine(" e1 == e2");
+            }
+            else
+            {
+                Console.WriteLine("e1 != e2");
+            }
+             //Функция
+             (int, int, int, char) func( int[] arr, string str45) 
+            {
+                return (arr.Max(), arr.Min(), arr.Sum(), str45[0]);
+            }
+
+            int[] ar = { 1, 2, 3 };
+            string str55 = "string";
+            (int, int, int, char) aa = func(ar, str55);
+            Console.WriteLine( aa);
+
+            // checked/unchecked
+            void fun1()
+            {
+                try
+                {
+                    checked
+                    {
+                        int x = int.MaxValue;
+                        x++;
+                    }
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Произошло переполнение!");
+                }
+            }
+
+            void fun2()
+            {
+                try
+                {
+                    unchecked // Не вызывает OverflowException
+                    {
+                        int x = int.MaxValue;
+                        x++;
+                    }
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Произошло переполнение!");
+                }
+            }
+
+            fun1();
+            fun2();
             Console.ReadKey();
-            
+
         }
     }
 }
